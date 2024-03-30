ed937d41e8dc991cd469f07c4099ad55ed01c252
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-30 22:56:15 +0800

    Hide player home button in player part

--- src/pages/parts/player/ScrapeErrorPart.tsx
+++ src/pages/parts/player/ScrapeErrorPart.tsx
@@ -108,14 +108,14 @@ export function ScrapeErrorPart(props: ScrapeErrorPartProps) {
         <Title>{t("player.scraping.notFound.title")}</Title>
         <Paragraph>{t("player.scraping.notFound.text")}</Paragraph>
         <div className="flex gap-3">
-          <Button
+          {/* <Button
             href="/"
             theme="secondary"
             padding="md:px-12 p-2.5"
             className="mt-6"
           >
             {t("player.scraping.notFound.homeButton")}
-          </Button>
+          </Button> */}
           <Button
             onClick={() => modal.show()}
             theme="purple"
